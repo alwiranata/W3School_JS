a0d4575189cfb6700e1ddc5d11102ce6c88cb7cd
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-12-07 21:54:02 +0700

    add : const.js

---
+++ js/const.js
@@ -0,0 +1,17 @@
+const name = "aldo"
+console.log(name)
+
+const  Programing = ["JS","PHP","C++"];
+
+Programing[0]="Python"
+
+Programing.push("Java")
+
+console.log(Programing)
+
+try {
+    console.log(name)
+    const name= "Aldo Wiranata" 
+} catch (error) {
+    console.log(error)
+}
